47398e54beabbe368ea5b24df88db00e4f88564e
Pablo Yohan <pabloyohandias@gmail.com>
2022-05-11 21:27:59 -0300

    Criacao de metodos para salvar tarefas já criadas

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,21 @@ function limpaInput() {
   inputTarefa.focus();
 }
 
+// salva as tarefas em um JSON
+function salvarTarefas() {
+  const liTarefas = tarefas.querySelectorAll('li');
+  const listaDeTarefas = [];
+
+  for (let tarefa of liTarefas) {
+    let tarefaTexto = tarefa.innerText;
+    tarefaTexto = tarefaTexto.replace('Apagar', '').trim();
+    listaDeTarefas.push(tarefaTexto);
+  }
+
+  const tarefasJSON = JSON.stringify(listaDeTarefas);
+  localStorage.setItem('tarefas', tarefasJSON);
+}
+
 function criaBotaoApagar(li) {
   li.innerText += " ";
   const botaoApagar = document.createElement('button');
@@ -28,8 +43,10 @@ function criaTarefa(textoInput) {
   tarefas.appendChild(li);
   limpaInput();
   criaBotaoApagar(li);
+  salvarTarefas();
 }
 
+// pega o evento de click no ENTER
 inputTarefa.addEventListener('keypress', function (e) {
   if (e.keyCode === 13) {
     if (!inputTarefa.value) return;
@@ -37,15 +54,31 @@ inputTarefa.addEventListener('keypress', function (e) {
   }
 })
 
+//click no botão de adicionar tarefa
 btnTarefa.addEventListener("click", function (e) {
   if (!inputTarefa.value) return;
   criaTarefa(inputTarefa.value);
 });
 
+//click no botão de remover tarefa
 document.addEventListener('click', function (e) {
   const el = e.target;
 
   if (el.classList.contains('apagar')) {
     el.parentElement.remove();
   }
+  salvarTarefas();
 })
+
+
+// coloca de volta as tarefas já salvas
+function adicionaTarefasSalvas() {
+  const tarefas = localStorage.getItem('tarefas');
+  const listaDeTarefas = JSON.parse(tarefas);
+
+  for (let tarefas of listaDeTarefas) {
+    criaTarefa(tarefas);
+  }
+}
+
+adicionaTarefasSalvas();
